bcbfcb730b16a0295c6a19915be6a597e3929ad9
Gergő Móricz <mo.geryy@gmail.com>
2024-01-08 16:30:32 +0100

    add db integration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
-export default function Home() {
+import { PrismaClient } from "@prisma/client";
+
+export default async function Home() {
+  const prisma = new PrismaClient();
   return (
     <main className="p-4">
       <h1 className="text-center text-2xl font-bold">mog-template</h1>
+      <p>
+        {await prisma.post.count()} posts, {await prisma.user.count()} users
+      </p>
     </main>
   );
 }
